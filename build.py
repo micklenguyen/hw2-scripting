5978313769133e595101d45bfe3cf61d94d4bda4
Mickle Nguyen <michael.nguyen724@gmail.com>
2019-07-02 21:37:02 -0700

    Updating build.py to move for loop into main()

--- build.py
+++ build.py
@@ -1,11 +1,16 @@
-def main(filename, output, title):
+def main():
 
-	# Read content of html pages
-	content = open(filename).read()
+	for page in pages:
+		filename = page['filename']
+		output = page['output']
+		title = page['title']
 
-	# Invoke function to return finished_page (base.html with filled in content)
-	finshed_page = apply_template(content, title)
-	write_html(output, finshed_page)
+		# Read content of html pages
+		content = open(filename).read()
+	
+		# Invoke function to return finished_page (base.html with filled in content)
+		finshed_page = apply_template(content, title)
+		write_html(output, finshed_page)
 
 
 def apply_template(content, title):
@@ -41,8 +46,6 @@ def write_html(output, finshed_page):
 	open(output, "w+").write(finshed_page)
 
 
-
-
 # Content pages list
 pages = [
 	{
@@ -63,12 +66,6 @@ pages = [
 ]
 
 
+
 if __name__ == "__main__":
-
-	# for loop to loop through content on list and use variables to invoke main()
-	for page in pages:
-		filename = page['filename']
-		output = page['output']
-		title = page['title']
-
-		main(filename, output, title)
+	main()
