5964a311ffa28fa9247e35e6ebf4a0fa36dea8a6
Mickle Nguyen <michael.nguyen724@gmail.com>
2019-07-05 21:18:27 -0700

    Adding jinja updataes

--- build.py
+++ build.py
@@ -1,70 +1,76 @@
 def main():
 
+	pages = auto_fill_list()
+
 	for page in pages:
-		filename = page['filename']
+		filepath = page['filepath']
 		output = page['output']
 		title = page['title']
 
 		# Read content of html pages
-		content = open(filename).read()
+		content = open(filepath).read()
 	
 		# Invoke function to return finished_page (base.html with filled in content)
-		finshed_page = apply_template(content, title)
+		finshed_page = apply_template(content, title, pages)
 		write_html(output, finshed_page)
 
 
-def apply_template(content, title):
+def auto_fill_list():
+
+	import glob
+	import os
+
+	# Loop through files in the content/ directory and save paths as a list
+	all_html_files = glob.glob("content/*.html")
+	#print(all_html_files)
+
+	# Loop through the all_html_files list, modify and extract file_name and name_only from the path
+	pages = []
+	for file_path in all_html_files:
+
+		# Saving the path to a varaible (ex. content/resume.html)
+		file_path = file_path
+
+		# Removes the file path from the file name (ex. content/resume.html -> resume.html)
+		file_name = os.path.basename(file_path)
+
+		# Removes the file path from the file name (ex. content/resume.html -> resume.html)
+		file_name = os.path.basename(file_path)
+		#print(file_name)
+
+		# Split the name from the file extention (ex. resume.html -> resume)
+		name_only, extension = os.path.splitext(file_name)
+
+		# Build a list with dicts of content information
+		pages.append({
+			"filepath": file_path,
+			"title": name_only,
+			"output": "docs/" + file_name,
+			"filename": file_name
+			})
+
+	return pages
+
+def apply_template(content, title, pages):
+
+	from jinja2 import Template
 
 	# Read base.html and save to template
-	template = open("templates/base.html").read()
-
-	template = template.replace("{{title}}", title)
-
-	active = '>> '
-	blank = ""
-	if title == 'Bio':
-		template = template.replace("{{active_bio}}", active)
-		template = template.replace("{{active_resume}}", blank)
-		template = template.replace("{{active_about}}", blank)
-	elif title == 'Resume':
-		template = template.replace("{{active_resume}}", active)
-		template = template.replace("{{active_bio}}", blank)
-		template = template.replace("{{active_about}}", blank)
-	else:
-		template = template.replace("{{active_about}}", active)
-		template = template.replace("{{active_bio}}", blank)
-		template = template.replace("{{active_resume}}", blank)
-
-	# Replaces string tag with content from html files
-	finshed_page = template.replace("{{content}}", content)
-	return finshed_page
+	template_html = open("templates/base.html").read()
+	new_template = Template(template_html)
+	finished_page = new_template.render(
+		title=title,
+		content=content,
+		pages=pages,
+		)
+	
+	return finished_page
 
 
 def write_html(output, finshed_page):
 
 	# Writes complete html files
 	open(output, "w+").write(finshed_page)
-
-
-# Content pages list
-pages = [
-	{
-	"filename": "content/index.html",
-	"output": "docs/index.html",
-	"title": "Bio",
-	},
-	{
-	"filename": "content/resume.html",
-	"output": "docs/resume.html",
-	"title": "Resume",
-	},
-	{
-	"filename": "content/about.html",
-	"output": "docs/about.html",
-	"title": "About",
-	},
-]
-
 
 
 if __name__ == "__main__":
