97577643366ab301ed9241102f7ba5879a6579be
Mickle Nguyen <michael.nguyen724@gmail.com>
2019-07-06 00:02:58 -0700

    adding error handling when arguments are not included

--- manage.py
+++ manage.py
@@ -1,12 +1,14 @@
 import utils
 import sys
 
+# Error handling if argument is not passed
 if len(sys.argv) <= 1:
     print('''Usage:
 	Rebuild site: python manage.py build
 	Create new page: python manage.py new''')
     exit(1)
 
+# Select to choose build or create a new page
 print("This is argv:", sys.argv)
 command = sys.argv[1]
 print(command)
